ad9ac083d257556bc92ae062df2be61fcd47db82
assabbar <oudmalak016@gmail.com>
2025-12-23 14:04:02 +0100

    Fix health check paths - add /api context path to actuator endpoints

--- Devops/terraform/mysql.tf
+++ Devops/terraform/mysql.tf
@@ -18,3 +18,12 @@ resource "azurerm_mysql_flexible_database" "appdb" {
   charset             = "utf8mb4"
   collation           = "utf8mb4_unicode_ci"
 }
+
+# Firewall rule to allow all IP addresses (for testing - restrict in production)
+resource "azurerm_mysql_flexible_server_firewall_rule" "allow_all" {
+  name                = "AllowAllIPAddresses"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_mysql_flexible_server.mysql.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "255.255.255.255"
+}
